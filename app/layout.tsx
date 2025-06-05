9d60971208133ca4df29bc72eb06618803a7f946
Mario Aparcero <marioaparcero@hotmail.com>
2025-06-05 02:18:06 +0200

    Upd: Mejorar metadatos del layout con título y descripción más específicos

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,12 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Mario Aparcero',
-  description: 'Administrador de sistemas, Master Técnico en Ciberseguridad, DevOps y SRE',
+  title: 'Mario Aparcero | Systems Administrator',
+  // description: 'Soy Administrador de sistemas, Master Técnico en Ciberseguridad, DevOps y SRE',
+  // description: 'SysAdmin experto en Linux, DevOps y SRE. Apasionado por la ciberseguridad y la automatización de infraestructuras.',
+  // description: 'SysAdmin experto en Linux, automatización, ciberseguridad, nube e infraestructura. Aseguro sistemas robustos, seguros y eficientes en cualquier entorno.',
+  // description: 'SysAdmin experto en Linux. Apasionado por la ciberseguridad, la automatización de infraestructuras y la nube. Aseguro sistemas robustos, seguros y eficientes en cualquier entorno.',
+  description: 'SysAdmin experto en Linux. Apasionado por la ciberseguridad y la automatización de infraestructuras. Aseguro sistemas robustos, seguros y eficientes en cualquier entorno.',
   generator: 'marioaparcero.dev',
 }
 
